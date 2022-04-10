8b0b32d654e2fc079707833a4dacd310a7957749
peng_xiang <cxz0917001997@gmail.com>
2022-04-10 17:21:07 +0800

    clean code

--- lib.py
+++ lib.py
@@ -4,40 +4,41 @@ import imutils
 import cv2
 
 def getQrcode(image):
-    # 将输入图片裁剪到固定大小
+    # resize 500
     image = imutils.resize(image, height=500)
-    # 将输入转换为灰度图片
+    # 灰階
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cv2.imwrite('output/1-cvtColor.png', gray)
-    # 进行高斯模糊操作
+    # 高斯模糊
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     cv2.imwrite('output/2-GaussianBlur.png', blurred)
-    # 执行边缘检测
+    # 邊緣檢測
     edged = cv2.Canny(blurred, 50, 200, 255)
     cv2.imwrite('output/3-Canny.png', edged)
     
-    # 在边缘检测map中发现轮廓
+    # 在邊緣檢測map中取得輪廓
     cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    # 根据大小对这些轮廓进行排序
+    # 依大小排序輪廓
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
     displayCnt = None
     
-    # 循环遍历所有的轮廓
+    # 讀取所有輪廓
     for c in cnts:
-        # 对轮廓进行近似
+        # 對輪廓進行相似比對
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.02 * peri, True)
     
-        # 如果当前的轮廓有4个顶点，我们返回这个结果，即LCD所在的位置
+        # 找出有四個頂點的輪廓
         if len(approx) == 4:
             displayCnt = approx
             break
     
-    # 应用视角变换到LCD屏幕上
+    # 把圖切出來
     warped = four_point_transform(gray, displayCnt.reshape(4, 2))
     cv2.imwrite('output/origin-qrcode.png', warped)
 
+    # 二值化
     thresh = cv2.threshold(warped, 100, 255, cv2.THRESH_BINARY)[1]
     cv2.imwrite('output/threshold-qrcode.png', thresh)
     return thresh
