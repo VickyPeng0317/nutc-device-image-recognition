8b3eccc3b5c7a0af5bef9aa51227a0e853e67418
peng_xiang <cxz0917001997@gmail.com>
2022-04-12 20:24:48 +0800

    feat 取得 LCD 區塊

--- lib.py
+++ lib.py
@@ -66,23 +66,23 @@ def getQrcode(image):
 
 def getNumber(image):
     (B, R, G) = cv2.split(image)
-    cv2.imwrite('output/1-B.png', B)
-    cv2.imwrite('output/1-R.png', R)
-    cv2.imwrite('output/1-G.png', G)
+    cv2.imwrite('output/getNumber/ORIGIN.png', image)
+    cv2.imwrite('output/getNumber/B.png', B)
+    cv2.imwrite('output/getNumber/R.png', R)
+    cv2.imwrite('output/getNumber/G.png', G)
     # 高斯模糊
     blurred = cv2.GaussianBlur(B, (5, 5), 0)
-    cv2.imwrite('output/2-GaussianBlur.png', blurred)
+    cv2.imwrite('output/getNumber/R_1-blurred.png', blurred)
     # 邊緣檢測
     edged = cv2.Canny(blurred, 50, 200, 255)
-    cv2.imwrite('output/3-Canny.png', edged)
+    cv2.imwrite('output/getNumber/R_2-Canny.png', edged)
 
         # 在邊緣檢測map中取得輪廓
     cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     # 依大小排序輪廓
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-    displayCnt = None
-    arr = []
+    allApprox = []
     # 讀取所有輪廓
     for c in cnts:
         # 對輪廓進行相似比對
@@ -91,15 +91,22 @@ def getNumber(image):
     
         # 找出有四個頂點的輪廓
         if len(approx) == 4:
-            displayCnt = approx
-            arr.append(approx)
-    i = 0
-    for c in arr:
-        # 把圖切出來
-        warped = four_point_transform(R, c.reshape(4, 2))
-        cv2.imwrite(f'test/{i}.png', warped)
-        # print(cv2.contourArea(arr[i]))
-        i = i + 1
+            allApprox.append(approx)
+
+    # 計算區域面積
+    allApproxArea = [cv2.contourArea(approx) for approx in allApprox]
+    # 取得最大面積索引
+    lcdAreaIndex = np.argmax(allApproxArea, axis=0)
+    # 切割 LCD 區塊
+    lcdAreaImg = four_point_transform(R, allApprox[lcdAreaIndex].reshape(4, 2))
+    cv2.imwrite(f'output/getNumber/LCD.png', lcdAreaImg)
+
+    # for c in arr:
+    #     # 把圖切出來
+    #     warped = four_point_transform(R, c.reshape(4, 2))
+    #     cv2.imwrite(f'output/getNumber/contourArea/{i}.png', warped)
+    #     print(cv2.contourArea(arr[i]))
+    #     i = i + 1
         # # 二值化
         # thresh = cv2.threshold(warped, 100, 255, cv2.THRESH_BINARY)[1]
         # cv2.imwrite('output/QR/threshold-qrcode.png', thresh)
