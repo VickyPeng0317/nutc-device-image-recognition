f16c7834b97ac69b1ab434864d65281c66cff0cb
彭浚翔 <cxz0917001997@gmail.com>
2022-04-25 15:23:29 +0800

    add test qrcode func

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from logic import getQrcodeImg, getLCDImg, getLCDNum
 from base import pipe, trans  
 from PIL import Image
 from pyzbar.pyzbar import decode
+import os
+import shutil
+
 
 # 讀取圖片
 image = cv2.imread("img/ccc2.png")
@@ -17,3 +20,23 @@ print(f'QRCode data:{qrcodeData}')
 
 lcdImg = getLCDImg(image)
 num = getLCDNum(lcdImg)
+
+
+def test_qrcode(count = 50):
+    shutil.rmtree('output/FAILQR')
+    os.mkdir('output/FAILQR')
+
+    success = []
+    fail = []
+    for i in range(count):
+        try:
+            image = cv2.imread(f"img/{str(i+1)}.png")
+            qrcodeImg = getQrcodeImg(image)
+            qrcodeData = decode(qrcodeImg)[0].data
+            success.append(i+1)
+        except:
+            cv2.imwrite(f'output/FAILQR/{str(i+1)}-qrcode.png', qrcodeImg)
+            fail.append(i+1)
+        
+    print(f'success: {len(success)}, fail: {len(fail)}')
+    print(fail)
