9879aaa93be622336cafeee43f04457d562292aa
peng_xiang <cxz0917001997@gmail.com>
2022-05-29 21:38:53 +0800

    modify db service

--- dbService.py
+++ dbService.py
@@ -1,7 +1,7 @@
 def selectDevice(deviceId):
     deviceList = [
-        {"deviceId": 1, "deviceName": "血壓機01", "deviceAddress": "資訊大樓1F電梯口"},
-        {"deviceId": 2, "deviceName": "血壓機02", "deviceAddress": "學校大門入口處"},
-        {"deviceId": 3, "deviceName": "血壓機03", "deviceAddress": "資訊大樓2501研究室"}
+        {"deviceId": 1, "deviceName": "體溫量測設備", "deviceAddress": "資訊大樓-2501", "type": "體溫影像辨識"},
+        {"deviceId": 2, "deviceName": "血壓機02", "deviceAddress": "學校大門入口處", "type": "血壓影像辨識"},
+        {"deviceId": 3, "deviceName": "血壓機03", "deviceAddress": "資訊大樓2501研究室", "type": "體溫影像辨識"}
     ]
     return [device for device in deviceList if device.get('deviceId')==deviceId][0]
